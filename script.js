c771317f3e10b482cab813959986a9beb3da339d
JuanRoccia <juanroccia@gmail.com>
2024-08-22 19:14:03 -0300

    script update

--- script.js
+++ script.js
@@ -1,3 +1,204 @@
+// Funcionalidades de forms.html
+document.addEventListener('DOMContentLoaded', function() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const forms = urlParams.getAll('form');
+    const formNames = urlParams.getAll('name');
+    const formContainer = document.getElementById('formContainer');
+    const formList = document.getElementById('formList');
+    const prevBtn = document.getElementById('prevBtn');
+    const nextBtn = document.getElementById('nextBtn');
+    const pageIndicator = document.getElementById('pageIndicator');
+    let currentFormIndex = 0;
+
+    function createFormList() {
+        const list = document.createElement('ul');
+        list.className = 'list-disc pl-5 mb-4';
+        formNames.forEach((name, index) => {
+            const listItem = document.createElement('li');
+            listItem.textContent = decodeURIComponent(name);
+            listItem.className = 'mb-1';
+            list.appendChild(listItem);
+        });
+        formList.appendChild(list);
+    }
+
+    function createIframes() {
+        forms.forEach((form, index) => {
+            const iframe = document.createElement('iframe');
+            iframe.src = `${form}.html`;
+            iframe.className = 'w-full h-full border-0 top-0 left-0 transition-opacity duration-300';
+            iframe.style.display = index === 0 ? 'block' : 'none';
+            iframe.style.opacity = index === 0 ? '1' : '0';
+            
+            iframe.addEventListener('load', function() {
+                adjustIframeHeight(this);
+                setupFormValidation(this, index);
+            });
+
+            iframe.addEventListener('error', function() {
+                console.error(`Error al cargar el formulario: ${form}.html`);
+                // Manejar el error, tal vez mostrar un mensaje al usuario
+            });
+            
+            formContainer.appendChild(iframe);
+        });
+    }
+
+    function adjustIframeHeight(iframe) {
+        iframe.style.height = iframe.contentWindow.document.body.scrollHeight + 'px';
+        iframe.contentWindow.document.documentElement.style.overflow = 'hidden';
+        iframe.contentWindow.document.documentElement.style.scrollbarWidth = 'none';
+        iframe.contentWindow.document.documentElement.style.webkitScrollbar = 'display: none';
+        
+        const resizeObserver = new ResizeObserver(() => {
+            iframe.style.height = iframe.contentWindow.document.body.scrollHeight + 'px';
+        });
+        resizeObserver.observe(iframe.contentWindow.document.body);
+    }
+
+    function setupFormValidation(iframe, index) {
+        const iframeDocument = iframe.contentWindow.document;
+        const requiredInputs = iframeDocument.querySelectorAll('input[required], select[required], textarea[required]');
+    
+        requiredInputs.forEach(input => {
+            input.addEventListener('change', () => checkFormCompleteness(iframe));
+        });
+
+        // Verificar completitud inicial
+        checkFormCompleteness(iframe);
+    }
+
+    function checkFormCompleteness(iframe) {
+        const iframeDocument = iframe.contentWindow.document;
+        const requiredInputs = iframeDocument.querySelectorAll('input[required], select[required], textarea[required]');
+        console.log('requiredInputs:', requiredInputs);
+
+        let isComplete = true;
+        requiredInputs.forEach(input => {
+            if (input.type === 'checkbox' || input.type === 'radio') {
+                const name = input.name;
+                const checkedInputs = iframeDocument.querySelectorAll(`input[name="${name}"]:checked`);
+                if (checkedInputs.length === 0) {
+                    console.log(`Campo no completado: ${input.name}`);
+                    isComplete = false;
+                }
+            } else if (!input.value.trim()) {
+                console.log(`Campo no completado: ${input.name || input.id}`);
+                isComplete = false;
+            }
+        });
+
+        // nextBtn.disabled = !isComplete;
+        return isComplete;
+    }
+
+    function updatePageIndicator() {
+        pageIndicator.textContent = `Página ${currentFormIndex + 1} de ${forms.length}`;
+    }
+
+    function updateButtonText() {
+        nextBtn.textContent = forms.length === 1 || currentFormIndex === forms.length - 1 ? 'Finalizar' : 'Siguiente';
+    }
+
+    function showForm(index) {
+        const iframes = formContainer.getElementsByTagName('iframe');
+        Array.from(iframes).forEach((iframe, i) => {
+            if (i === index) {
+                iframe.style.display = 'block';
+                setTimeout(() => { iframe.style.opacity = '1'; }, 50);
+            } else {
+                iframe.style.opacity = '0';
+                setTimeout(() => { iframe.style.display = 'none'; }, 300);
+            }
+        });
+        currentFormIndex = index;
+        updatePageIndicator();
+        prevBtn.disabled = currentFormIndex === 0;
+        // nextBtn.textContent = currentFormIndex === forms.length - 1 ? 'Finalizar' : 'Siguiente';
+        updateButtonText();
+    }
+
+    prevBtn.addEventListener('click', () => {
+        if (currentFormIndex > 0) {
+            showForm(currentFormIndex - 1);
+        }
+    });
+
+    nextBtn.addEventListener('click', () => {
+        // alert('Estas tocando el boton siguiente');
+        const currentIframe = formContainer.getElementsByTagName('iframe')[currentFormIndex];
+        const iframeDocument = currentIframe.contentWindow.document;
+        const requiredInputs = iframeDocument.querySelectorAll('input[required], select[required], textarea[required]');
+        
+        if (currentIframe && checkFormCompleteness(currentIframe)) {
+            if (currentFormIndex < forms.length - 1) {
+                showForm(currentFormIndex + 1);
+            } else {
+                finalizeProcess();
+                // alert('Has completado todos los formularios.');
+                // downloadForms();
+                // Descarga los formularios en formato PDF y jpg
+                // prepareWhatsAppShare();
+                // Enviar los formularios por whatsapp
+
+            }
+        } else {
+            let incompleteFields = [];
+            requiredInputs.forEach(input => {
+                if ((input.type === 'checkbox' || input.type === 'radio') && !iframeDocument.querySelector(`input[name="${input.name}"]:checked`)) {
+                    incompleteFields.push(input.name);
+                } else if (!input.value.trim()) {
+                    incompleteFields.push(input.name || input.id);
+                }
+            });
+            alert(`Por favor, completa los siguientes campos antes de continuar: ${incompleteFields.join(', ')}`);
+        }
+    });
+
+    function finalizeProcess() {
+        alert('Has completado todos los formularios.');
+        downloadForms().then(() => {
+            prepareWhatsAppShare();
+        });
+    }
+
+    async function downloadForms() {
+        for (let index = 0; index < forms.length; index++) {
+            const iframe = formContainer.getElementsByTagName('iframe')[index];
+            const iframeContent = iframe.contentWindow.document.body;
+
+            // Convert to PDF
+            await html2pdf().from(iframeContent).save(`form_${index + 1}.pdf`);
+
+            // Convert to JPG
+            const canvas = await html2canvas(iframeContent);
+            const link = document.createElement('a');
+            link.download = `form_${index + 1}.jpg`;
+            link.href = canvas.toDataURL('image/jpeg');
+            link.click();
+        }
+    }
+
+    function prepareWhatsAppShare() {
+        // Aquí puedes implementar la lógica para compartir por WhatsApp
+        // Por ejemplo, abrir un nuevo enlace de WhatsApp con un mensaje predefinido
+        const message = encodeURIComponent('He completado los formularios. Aquí están adjuntos los archivos PDF y JPG.');
+        const whatsappLink = `https://wa.me/?text=${message}`;
+        window.open(whatsappLink, '_blank');
+    }
+
+    if (forms.length === 0) {
+        formContainer.innerHTML = '<p class="text-red-500">No se han seleccionado formularios.</p>';
+    } else {
+        createFormList();
+        createIframes();
+        updatePageIndicator();
+        updateButtonText();
+    }
+
+});
+// FIN Funcionalidades de forms.html
+
 const loginForm = document.getElementById('loginForm');
 const dashboardContent = document.getElementById('dashboardContent');
 const loginButton = document.getElementById('loginButton');
@@ -30,9 +231,6 @@ if (document.getElementById('copyButton')) {
     const copyButton = document.getElementById('copyButton');
     copyButton.addEventListener('click', copyToClipboard);
 }
-// loginButton.addEventListener('click', login);
-// generateButton.addEventListener('click', generateLink);
-// copyButton.addEventListener('click', copyToClipboard);
 
 function login() {
     const username = usernameInput.value;
@@ -66,12 +264,12 @@ function addSignatureToLastForm() {
     const lastForm = selectedForms[selectedForms.length - 1];
     const signatureHtml = `
         <!-- Sección de Firma -->
-        <div class="mb-6">
-          <h2 class="text-2xl font-bold mb-4">Firma</h2>
-          <div class="">
+        <div class="mb-6 border-b border-gray-900/10 pb-12">
+          <h2 class="text-2xl font-bold mb-4 text-base font-semibold leading-7 text-gray-900">Firma</h2>
+          <div class="mt-10">
             <label for="signature-pad" class="block text-sm font-medium leading-6 text-gray-900">Firma del paciente</label>
             <div class="mt-2">
-              <canvas id="signature-pad" class="border rounded-md"></canvas>
+              <canvas id="signature-pad" class="border rounded-md w-full h-32"></canvas>
             </div>
             <button type="button" id="clear" class="mt-2 rounded-md bg-red-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-400">Limpiar</button>
           </div>
