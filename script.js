627bd6a957ea0238c84dae9d2e40d09f157ea054
JuanRoccia <juanroccia@gmail.com>
2024-10-16 21:09:58 -0300

    url netlify

--- script.js
+++ script.js
@@ -484,9 +484,9 @@ function generateLink() {
 
     // addSignatureToLastForm();
 
-    // const baseUrl = 'https://imagenesalem.netlify.app/index.html?';
+    const baseUrl = 'https://imagenesalem.netlify.app/index.html?';
     // const baseUrl = 'https://www.imagenesalem.com/index.html?';
-    const baseUrl = 'https://www.imagenesalem.com/dashboard/index.html?';
+    // const baseUrl = 'https://www.imagenesalem.com/dashboard/index.html?';
     const formParams = selectedForms.map(form => `form=${form.value}&name=${encodeURIComponent(form.name)}`).join('&');
     const fullLink = `${baseUrl}${formParams}`;
 
